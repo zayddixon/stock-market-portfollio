b6a753ef9f6482f76046eec79c4811fa0660c386
Zayd Dixon <zayd.dixon@mrjaffesclass.com>
2022-04-22 19:25:52 +0000

    Replaced code in App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
+import { Card, CardHeader, CardBody, CardFooter, Button } from 'reactstrap';
+import sampleData from './sampleData';
+import StockList from './StockList';
 
 function App() {
+  
+  // Uncomment setMyName if required, for example, if the name
+  // is stored in the DynamoDB
+  const [myName/*, setMyName*/] = useState('Roger');
+  
+  const [stocks, setStocks] = useState([]);
+  const [stockList, setStockList] = useState([]);
+  
+  // Retrieve the current stock information when the page first loads
+  useEffect(() => {
+    setStocks(sampleData);
+  }, []);
+  
+  // With the stock data add purchase value, current price
+  // and current value to the stock record
+  useEffect(() => {
+    const enhancedStocks = stocks.map(stock => {
+      stock.purchaseValue = stock.shares * stock.purchasePrice;
+      stock.currentPrice = Math.random()*200 + 50;
+      stock.currentValue = stock.shares * stock.currentPrice;
+      stock.profit = stock.currentValue - stock.purchaseValue;
+      return stock;
+    })
+    setStockList(enhancedStocks);
+  }, [stocks])
+  
+  const addStock = evt => {
+    console.log('add stock clicked');
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload!!!!!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Card>
+        <CardHeader className="card-header-color">
+          <h4>{myName}'s Stock Portfolio</h4>
+        </CardHeader>
+        <CardBody>
+          <StockList data={stockList} />
+        </CardBody>
+        <CardFooter>
+          <Button size="sm" onClick={addStock}>Add stock</Button>
+        </CardFooter>
+      </Card>
     </div>
   );
 }
