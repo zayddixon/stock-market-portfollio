47840b289c72e3c6a1ccfd42d5a3be15d8422230
Zayd Dixon <zayd.dixon@mrjaffesclass.com>
2022-04-25 19:23:06 +0000

    Add sample data

---
+++ src/sampleData.js
@@ -0,0 +1,8 @@
+const sampleData = [
+  {ticker: 'AAPL', shares: 100, purchasePrice: 45.00, name: 'Apple'},
+  {ticker: 'MSFT', shares: 50, purchasePrice: 125.00, name: 'Microsoft'},
+  {ticker: 'GOOG', shares: 750, purchasePrice: 375.00, name: 'Alphabet'},
+  {ticker: 'KO', shares: 95, purchasePrice: 28.00, name: 'Coca Cola'},
+];
+
+export default sampleData;
